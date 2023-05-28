1bff474979cb26b3e6cf8e0bc8f42943240e6c3f
nmurillo3115@gmail.com <nmurillo3115@gmail.com>
2023-05-28 17:42:28 -0500

    update

---
+++ JS/titulos.js
@@ -0,0 +1,29 @@
+var titulo1 = document.getElementById('titulo1');
+var titulo2 = document.getElementById('titulo2');
+var titulo3 = document.getElementById('titulo3');
+var titulo4 = document.getElementById('titulo4');
+var titulo5 = document.getElementById('titulo5');
+var titulo6 = document.getElementById('titulo6');
+var titulo7 = document.getElementById('titulo7');
+var titulo8 = document.getElementById('titulo8');
+
+
+titulo1.addEventListener('click', function(){
+    window.location.href = 'calculadora.html';
+});
+titulo2.addEventListener('click', function(){
+    window.location.href = 'app-movil.html';
+});
+titulo3.addEventListener('click', function(){
+    window.location.href = 'info.html';
+});
+titulo4.addEventListener('click', function(){
+    window.location.href = 'Economia-circular.html';
+});
+titulo5.addEventListener('click', function(){
+    window.location.href = 'puntos.html';
+});
+titulo6.addEventListener('click', function(){
+    window.location.href = 'CasaReciclaje.html';
+});
+
